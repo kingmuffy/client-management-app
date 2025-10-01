53cd860ffdc1b6d801719bdc3e2d4b4a9c352ba9
kingmuffy <kingmuffyatse@gmail.com>
2025-10-01 09:36:34 +0200

    Initial commit: Spring Boot backend and Angular frontend

---
+++ backend/src/main/java/com/omamofe/clientmanagement/ClientManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.omamofe.clientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientManagementBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/omamofe/clientmanagement/ClientManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omamofe.clientmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
