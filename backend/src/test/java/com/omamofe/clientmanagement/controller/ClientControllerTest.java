8f391dfb2f3d265f52aebf3094364cc2fd60243b
kingmuffy <kingmuffyatse@gmail.com>
2025-10-05 23:57:56 +0200

    allow for testing

--- backend/src/test/java/com/omamofe/clientmanagement/controller/ClientControllerTest.java
+++ backend/src/test/java/com/omamofe/clientmanagement/controller/ClientControllerTest.java
@@ -7,6 +7,7 @@ import com.omamofe.clientmanagement.service.ClientService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ClientController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class ClientControllerTest {
 
     @Autowired
